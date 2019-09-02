360f7dfc89d6fd26a950ffd7d90e40c9b047a128
adrianFarkas <farkasadrian1996@gmail.com>
2019-09-02 13:49:30 +0200

    we will win this

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,13 +8,15 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0.5";
+    static final String VERSION = "1.0.6";
 
     public static int betRequest(JsonElement request) {
         JsonObject gameState = request.getAsJsonObject();
         JsonArray communityCards = gameState.get("community_cards").getAsJsonArray();
         int currentBuyIn = gameState.get("current_buy_in").getAsInt();
-        if(currentBuyIn > 400) return check();
+
+        if(currentBuyIn > 400 && !hasHandPair(gameState)) return check();
+
         if(communityCards.size() > 0) {
             if (checkCards(gameState)) return call(gameState);
             else return check();
@@ -44,7 +46,6 @@ public class Player {
     }
 
     public static boolean hasPair(JsonArray holeCards, JsonArray communityCards) {
-        if(holeCards.get(0).getAsJsonObject().get("rank").equals(holeCards.get(1).getAsJsonObject().get("rank"))) return true;
         for (int i = 0; i < holeCards.size(); i++) {
             JsonObject card = holeCards.get(i).getAsJsonObject();
             for (int j = 0; j < communityCards.size(); j++) {
@@ -53,5 +54,11 @@ public class Player {
             }
         }
         return false;
+    }
+
+    public static boolean hasHandPair(JsonObject gameState) {
+        JsonObject player = gameState.get("players").getAsJsonArray().get(gameState.get("in_action").getAsInt()).getAsJsonObject();
+        JsonArray holeCards = player.get("hole_cards").getAsJsonArray();
+        return holeCards.get(0).getAsJsonObject().get("rank").equals(holeCards.get(1).getAsJsonObject().get("rank"));
     }
 }
