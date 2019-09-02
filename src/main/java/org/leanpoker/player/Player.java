c02686ade58d7b2b8a5b7e0479f1eb989f65e943
adrianFarkas <farkasadrian1996@gmail.com>
2019-09-02 10:50:19 +0200

    call every bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +10,10 @@ public class Player {
     static final String VERSION = "1.0.0";
 
     public static int betRequest(JsonElement request) {
-        return 100;
+        JsonObject gameState = request.getAsJsonObject();
+        int currentBuyIn = gameState.get("currentÍ_buy_in").getAsInt();
+        int bet = gameState.get("players").getAsJsonArray().get(gameState.get("in_action").getAsInt()).getAsJsonObject().get("bet").getAsInt();
+        return currentBuyIn - bet;
     }
 
     public static void showdown(JsonElement game) {
